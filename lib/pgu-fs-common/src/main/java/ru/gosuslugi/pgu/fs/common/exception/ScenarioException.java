e2f0db6b0c9dcba76d623437af103bddf1566333
anndragan@it-one.ru <anndragan@it-one.ru>
2022-01-18 13:55:55 +0300

    Init commit

--- lib/pgu-fs-common/src/main/java/ru/gosuslugi/pgu/fs/common/exception/ScenarioException.java
+++
@@ -1,8 +0,0 @@
-package ru.gosuslugi.pgu.fs.common.exception;
-
-public class ScenarioException extends FormBaseWorkflowException {
-
-    public ScenarioException(String s) {
-        super(s);
-    }
-}
--- lib/pgu-fs-common/src/main/java/ru/gosuslugi/pgu/fs/common/exception/TargetIdException.java
+++
@@ -1,8 +0,0 @@
-package ru.gosuslugi.pgu.fs.common.exception;
-
-public class TargetIdException extends FormBaseWorkflowException {
-
-    public TargetIdException(String s) {
-        super(s);
-    }
-}
