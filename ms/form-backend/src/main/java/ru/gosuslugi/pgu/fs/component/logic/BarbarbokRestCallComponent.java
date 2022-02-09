2db4de459853d3b3846a83bfc7ea8a12cdd2cc9f
Levgenix <mr.lobach@gmail.com>
2022-02-09 14:10:10 +0300

    EPGUCORE-87878 [DEV][BE] Исправить ошибки BarbarbokRestCall

--- ms/form-backend/src/main/java/ru/gosuslugi/pgu/fs/component/logic/BarbarbokRestCallComponent.java
+++ ms/form-backend/src/main/java/ru/gosuslugi/pgu/fs/component/logic/BarbarbokRestCallComponent.java
@@ -2,8 +2,13 @@ package ru.gosuslugi.pgu.fs.component.logic;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMethod;
+import ru.gosuslugi.pgu.common.core.json.JsonProcessingUtil;
 import ru.gosuslugi.pgu.common.esia.search.dto.UserPersonalData;
 import ru.gosuslugi.pgu.dto.ScenarioDto;
+import ru.gosuslugi.pgu.dto.SmevConverterGetRequestDto;
+import ru.gosuslugi.pgu.dto.SmevConverterPullRequestDto;
+import ru.gosuslugi.pgu.dto.SmevConverterPushRequestDto;
 import ru.gosuslugi.pgu.dto.descriptor.FieldComponent;
 import ru.gosuslugi.pgu.dto.descriptor.types.ComponentType;
 import ru.gosuslugi.pgu.fs.common.component.ComponentResponse;
@@ -11,6 +16,10 @@ import ru.gosuslugi.pgu.fs.service.BackRestCallService;
 
 import java.util.Map;
 
+import static ru.gosuslugi.pgu.components.ComponentAttributes.BODY_ATTR;
+import static ru.gosuslugi.pgu.components.ComponentAttributes.METHOD_ATTR;
+import static ru.gosuslugi.pgu.components.ComponentAttributes.PATH_ATTR;
+import static ru.gosuslugi.pgu.components.ComponentAttributes.SERVICE_ID_ATTR;
 import static ru.gosuslugi.pgu.components.ComponentAttributes.URL_ATTR;
 
 /**
@@ -35,14 +44,31 @@ public class BarbarbokRestCallComponent extends BackRestCallComponent {
 
     @Override
     public ComponentResponse<String> getInitialValue(FieldComponent component, ScenarioDto scenarioDto) {
-        component.getAttrs().putAll(
+        prepareAttrs(component, scenarioDto);
+        return super.getInitialValue(component, scenarioDto);
+    }
+
+    private void prepareAttrs(FieldComponent component, ScenarioDto scenarioDto) {
+        Map<String, Object> attrs = component.getAttrs();
+        attrs.put(SERVICE_ID_ATTR, scenarioDto.getServiceCode());
+        attrs.putAll(
                 Map.of(
+                        METHOD_ATTR, attrs.getOrDefault(METHOD_ATTR, RequestMethod.POST),
                         URL_ATTR, smevConverterUrl,
+                        PATH_ATTR, attrs.getOrDefault(PATH_ATTR, "/services/get"),
                         ESIA_AUTH_ATTR, true
                 )
         );
-
-        return super.getInitialValue(component, scenarioDto);
+        if (RequestMethod.GET.toString().equalsIgnoreCase(attrs.get(METHOD_ATTR).toString())) {
+            attrs.put(BODY_ATTR, JsonProcessingUtil.toJson(objectMapper.convertValue(attrs, SmevConverterPullRequestDto.class)));
+        } else {
+            var templateName = attrs.getOrDefault("templateName", null);
+            if (templateName == null) {
+                attrs.put(BODY_ATTR, JsonProcessingUtil.toJson(objectMapper.convertValue(attrs, SmevConverterPushRequestDto.class)));
+            } else {
+                attrs.put(BODY_ATTR, JsonProcessingUtil.toJson(objectMapper.convertValue(attrs, SmevConverterGetRequestDto.class)));
+            }
+        }
     }
 
     @Override
