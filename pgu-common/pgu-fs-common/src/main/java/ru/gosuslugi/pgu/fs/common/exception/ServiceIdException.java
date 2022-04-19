8a0a2fe6babc9d0c7ff5d1f9b39c5e66390fdae2
Vyacheslav Mayorov <vyacheslav.mayorov@gmail.com>
2022-04-19 16:36:50 +0300

    EPGUCORE-93776: Оптимизация связности sf-lib и form service

--- pgu-common/pgu-fs-common/src/main/java/ru/gosuslugi/pgu/fs/common/exception/ServiceIdException.java
+++
@@ -1,8 +0,0 @@
-package ru.gosuslugi.pgu.fs.common.exception;
-
-public class ServiceIdException extends FormBaseWorkflowException {
-
-    public ServiceIdException(String s) {
-        super(s);
-    }
-}
--- pgu-common/pgu-fs-common/src/main/java/ru/gosuslugi/pgu/fs/common/exception/dto/StatusIcon.java
+++
@@ -1,5 +0,0 @@
-package ru.gosuslugi.pgu.fs.common.exception.dto;
-
-public enum StatusIcon {
-    error, warning, stop
-}
