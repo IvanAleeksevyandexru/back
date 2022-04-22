2912d1636c02ef6a860a8f2c780b2dee3a324e8e
Aleksandr Gurin <agurin@it-one.ru>
2022-04-22 12:07:18 +0300

    EPGUCORE-77379: Реализовать возможность отображения в консоли значений protectedFields на этапе разработки и отладки + описать значения в protectedFields в конфлюенс

--- lib/pgu-common-fs/src/main/java/ru/gosuslugi/pgu/fs/common/service/ProtectedFieldService.java
+++ lib/pgu-common-fs/src/main/java/ru/gosuslugi/pgu/fs/common/service/ProtectedFieldService.java
@@ -1,7 +1,9 @@
 package ru.gosuslugi.pgu.fs.common.service;
 
+import java.util.Map;
+
 public interface ProtectedFieldService {
 
     Object getValue(String name);
-
+    Map<String, Object> getAllValues();
 }
